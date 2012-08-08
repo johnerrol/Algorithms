cb73a9bcec6e2798cd2b0ab6034f97580a7998db
johnerrol <riverajohnerrol@yahoo.com>
2012-08-08 13:49:19 +0800

    swapping 1 and swapping 2,swapping 3

---
+++ SWAPPING 2.py
@@ -0,0 +1,15 @@
+def Swap(Dataset,IndexToSwap1,IndexToSwap2):
+    temp=Dataset[IndexToSwap1]
+    Dataset[IndexToSwap1]=Dataset[IndexToSwap2]
+    Dataset[IndexToSwap2]=temp
+    return Dataset
+Dataset=["Ko","Bahay","Kubo","Aso","Payong","Puno"]
+IndexStart=0
+for IndexToCompare1 in range(IndexStart,len(Dataset)):
+    for IndexToCompare2 in range(IndexToCompare1,len(Dataset)):
+        if (Dataset[IndexToCompare1].upper())>(Dataset[IndexToCompare2].upper()):
+            Dataset=Swap(Dataset,IndexToCompare1,IndexToCompare2)
+       
+
+
+print(Dataset)
