3e2a2213dd9871fad600e6c74419c75c507bf02a
johnerrol <riverajohnerrol@yahoo.com>
2012-08-08 13:17:48 +0800

    casesensitive

---
+++ casesensitive.py
@@ -0,0 +1,9 @@
+def LinearSearch(Dataset, keyword):
+    for Data in Dataset:
+        if (Data.lower()==keyword.lower()):
+            return True
+    return False
+
+Dataset=["Arc", "Ball", "Court"]
+result=LinearSearch(Dataset,"BALL")
+print(result)
